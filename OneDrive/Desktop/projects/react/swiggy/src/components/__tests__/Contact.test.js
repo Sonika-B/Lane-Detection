5e1c03e242cbe37e90a4bc28c3c595279a53b025
Sonika Batchu <101943808+Sonika-B@users.noreply.github.com>
2024-07-17 08:59:16 +0530

    Delete OneDrive/Desktop/projects/react/swiggy directory

--- OneDrive/Desktop/projects/react/swiggy/src/components/__tests__/Contact.test.js
+++
@@ -1,9 +0,0 @@
-import Contact from "../Contact"
-import {screen,render} from "@testing-library/react"
-import "@testing-library/jest-dom"
-
-test("should load contact us page",()=>{
-    render(<Contact/>);
-    const heading = screen.getByRole("heading");
-    expect(heading).toBeInTheDocument();
-})
--- OneDrive/Desktop/projects/react/swiggy/src/components/__tests__/sum.test.js
+++
@@ -1,6 +0,0 @@
-import { sum } from "../sum";
-
-test("sum function should calculate the sum of 2 functions",()=>{
-    const result=sum(3,4);
-    expect(result).toBe(7);
-})
